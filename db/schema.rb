8140e9757f3ae1b5d14fd04d76d4e8ac503a6744
jrmart12 <rodolfojose@live.com>
2018-02-19 17:49:51 -0600

    - Add the name column to the users table. - Include name attribute to sign_up_params and account_update_params
    git commit -m

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20180219215133) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
